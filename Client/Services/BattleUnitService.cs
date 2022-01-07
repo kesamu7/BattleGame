06f95207dd9a30c7ef635674fa7534393d11ca3f
kesamu7 <kesqui7@gmail.com>
2022-01-07 17:47:55 -0600

    Added more battle unit code.

--- Client/Services/BattleUnitService.cs
+++ Client/Services/BattleUnitService.cs
@@ -5,12 +5,18 @@ namespace BattleGame.Client.Services
     public class BattleUnitService : IBattleUnitService
     {
         //A UNIT IS A GROUP OF SPECIFIC FIGHTERS THAT YOU CAN CREATE OR GET LIKE ARCHERS,MAGES,BRAWLERS,KNIGHTS...ETC
-        public IList<BattleUnit> BattleUnits { get; set; }
-        public IList<UserUnit> UserUnits { get; set ; }
-
-        public void AddUnit(int UnitId)
+        public IList<BattleUnit> BattleUnits => new List<BattleUnit>
         {
-            throw new NotImplementedException();
+            new BattleUnit{Id = 1, Title = "Knight", Attack = 10, Defense = 10, PizzaSliceCost = 100},
+            new BattleUnit{Id = 2, Title = "Archer", Attack = 15, Defense=5, PizzaSliceCost = 150},
+            new BattleUnit(){Id = 3, Title = "Mage", Attack = 20, Defense = 10, PizzaSliceCost=200},
+        };
+        public IList<UserUnit> UserUnits { get; set ; } = new List<UserUnit>();
+
+        public void AddUnit(int unitId)
+        {
+            var unit = BattleUnits.First(unit => unit.Id == unitId);
+            UserUnits.Add(new UserUnit { UnitId = unit.Id, HitPoints = unit.HitPoints });
         }
     }
 }
--- Client/Services/IBattleUnitService.cs
+++ Client/Services/IBattleUnitService.cs
@@ -4,7 +4,7 @@ namespace BattleGame.Client.Services
 {
     public interface IBattleUnitService
     {
-        IList<BattleUnit> BattleUnits { get; set; }
+        IList<BattleUnit> BattleUnits { get; }
         IList<UserUnit> UserUnits { get; set; }
 
         void AddUnit(int UnitId);
